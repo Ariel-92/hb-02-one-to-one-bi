ceaaee84ad453ea3c4a011a5c908fe8d4acb47fe
EH-Son <eunha.son08@gmail.com>
2022-10-28 13:09:26 +0900

    delete only InstructorDetail practice

--- src/main/java/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
+++ src/main/java/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -25,7 +25,7 @@ public class DeleteInstructorDetailDemo {
             session.beginTransaction();
 
             // get the instructor detail object
-            int id = 2;
+            int id = 3;
             InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, id);
 
             // print the instructor detail
@@ -36,6 +36,11 @@ public class DeleteInstructorDetailDemo {
 
             // now let's delete the instructor detail
             System.out.println("Deleting tempInstructorDetail: " + tempInstructorDetail);
+
+            // remove the associated object reference
+            // break bi-directional link
+            tempInstructorDetail.getInstructor().setInstructorDetail(null);
+
             session.delete(tempInstructorDetail);
 
             // commit transaction
